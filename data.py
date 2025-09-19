cfd8b0968de93d0eaf808efb4a597d76ddadb15c
Ekaterina Nester <harlanova.ev@gmail.com>
2025-09-19 23:34:26 +0300

    Added files

---
+++ data.py
@@ -0,0 +1,11 @@
+# Данные пользователя для корректных логинов
+class Data:
+    name = 'ekaterinanester3031777'
+    email = 'ekaterinanester3031777@yandex.ru'
+    password = '123456'
+
+# Список URL для перехода на нужные страницы
+class UrlList:
+    page_main_url = "https://stellarburgers.nomoreparties.site/"  # URL главной страницы Конструктор
+    page_login_url = "https://stellarburgers.nomoreparties.site/login"  # URL страницы логин
+    page_registration_url = "https://stellarburgers.nomoreparties.site/register" # URL страницы регистрации
